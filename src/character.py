5d694fee2ecb1eab05f0618c99652e6f151918aa
Christian <christian.garcia@valpo.edu>
2022-10-12 15:39:00 -0500

    finished the constructors for character/movement class

--- src/character.py
+++ src/character.py
@@ -1,3 +1,4 @@
+from movement import Movement
 class Character:
     health: int
     current_weapon = None
@@ -6,3 +7,9 @@ class Character:
 
     def __init__(self, id: int, movement: Movement, max_health: int, default_weapon, special_ability, height:int):
         print("Hello! I am a character!")
+        self.id = id
+        self.movement = movement
+        self.max_health = max_health
+        self.default_weapon = default_weapon
+        self.special_ability = special_ability
+        
--- src/movement.py
+++ src/movement.py
@@ -6,3 +6,5 @@ class Movement:
 
     def __init__(self, movement_speed: int, jump_height: int):
         print("Hello, I am a thing that can move!")
+        self.movement_speed = movement_speed
+        self.jump_height = jump_height
