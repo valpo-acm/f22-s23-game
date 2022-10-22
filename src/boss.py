a62137ce45b643dc578960acb1af50d6b1781f60
Nathan Harmon <22229517+nathanharmon1@users.noreply.github.com>
2022-10-21 19:01:27 -0500

    Added characters and player objects during meeting. Modified boss and enemy classes.

--- src/boss.py
+++ src/boss.py
@@ -1,11 +1,11 @@
 from character import Character
-from movement import Movement
 from weapon import Weapon
 
 class Boss(Character):
-    def __init__(self, char_id: int, movement: Movement, max_health: int,
-                 default_weapon: Weapon, special_ability, height:int):
-        super().__init__(char_id, movement, max_health, default_weapon, special_ability, height)
+    def __init__(self, max_health: int, default_weapon: Weapon,
+                 special_ability, height: int, movement_speed: int, jump_height: int, ducking_height: int,
+                 sliding_height: int):
+        super().__init__(max_health, default_weapon, special_ability, height, movement_speed, jump_height, ducking_height, sliding_height)
         print("I am a boss enemy! Prepare for a real challenge!")
 
     def boss_attack(self):
--- src/movement.py
+++ src/movement.py
@@ -1,14 +1,33 @@
 class Movement:
 
     x_loc: int = 0
-    y_loc: int = 0
+    y_loc: int = 0 # should be located at the feed
     is_ducking = False
     is_sliding = False #can remove this later but just an idea
+    is_jumping = False
 
-    def __init__(self, movement_speed: int, jump_height: int, ducking_height: int,
+    def __init__(self, movement_speed: int, height: int, jump_height: int, ducking_height: int,
                  sliding_height: int):
         print("Hello, I am a thing that can move!")
         self.movement_speed = movement_speed
+        self.height = height
         self.jump_height = jump_height
         self.ducking_height = ducking_height
         self.sliding_height = sliding_height
+
+    def slide_start(self):
+        self.is_sliding = True
+
+    def slide_end(self):
+        # IF THERE IS A BLOCK ABOVE
+            # KEEP SLIDING
+        # ELSE
+        self.is_sliding = False
+
+    def get_current_height(self):
+        if self.is_sliding:
+            return self.sliding_height
+        elif self.is_ducking:
+            return self.ducking_height
+        else:
+            return self.height
